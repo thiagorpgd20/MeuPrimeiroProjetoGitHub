22a5142b850fb8990a2dd8d5689374fc28b8f815
Tiago <Tiago@PCTIAGO>
2017-07-02 09:39:09 -0300

    Primeira atualização da minha solução

---
+++ ALoGitHub/ALoGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro teste com o GitHub");
+            Console.ReadKey();
+        }
+    }
+}
